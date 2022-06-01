960f0f6e006d416525174068dbb16c9d1de5c0d3
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-06-01 14:40:17 +0100

    body reading changed

--- server.js
+++ server.js
@@ -111,12 +111,8 @@ async function handleNewPlant(req, res) {
 }
 
 async function handleHarvest(req, res) {
-  const plantID = req.body;
-  try {
-    await client.query(`UPDATE plants_in_garden SET harvested = true WHERE id = $1`, plantID);
-  } catch (e) {
-    return res.status(500).json({ error: e });
-  }
+  const { plantID } = req.body;
+  await client.query(`UPDATE plants_in_garden SET harvested = true WHERE id = $1`, plantID);
 
   res.status(200).json("Harvest registered!");
 }
