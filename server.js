d0c230801ce5eca2eeba6625971ac6d39d585c98
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-06-01 14:41:55 +0100

    queries are all in arrays now

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ async function handleNewPlant(req, res) {
 
 async function handleHarvest(req, res) {
   const { plantID } = req.body;
-  await client.query(`UPDATE plants_in_garden SET harvested = true WHERE id = $1`, plantID);
+  await client.query(`UPDATE plants_in_garden SET harvested = true WHERE id = $1`, [plantID]);
 
   res.status(200).json("Harvest registered!");
 }
