e4f96d34927c6f8a116eefb2555ba116581afbc8
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-06-07 16:59:56 +0100

    validate session working as intended

--- server.js
+++ server.js
@@ -48,8 +48,10 @@ app.listen(PORT, () => console.log("listening on port " + PORT));
 
 async function handleValidateSession(req, res) {
   const { sessionID } = req.body;
-  const sessions = (await client.query(`SELECT uuid FROM sessions`)).rows;
-  const isValid = sessions.includes(sessionID);
+  const session = (await client.query(`SELECT * FROM sessions WHERE uuid = $1`, [sessionID])).rows;
+
+  let isValid = Boolean(session.length);
+
   res.status(200).json({ response: isValid });
 }
 
