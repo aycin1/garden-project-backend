a246ae7f5bf1822d21677dceacd50b3c3dcb34cd
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-06-08 11:08:58 +0100

    removed /

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ client.connect();
 const corsSettings = {
   origin: [
     "http://localhost:3000",
-    "https://garden-project-website.sigmalabs.co.uk/",
+    "https://garden-project-website.sigmalabs.co.uk",
     "https://garden-project-website.sigmalabs.co.uk/plants-info",
   ],
   allowedHeaders: ["Access-Control-Allow-Origin", "Authorization", "Content-Type", "Accept", "Origin", "User-Agent"],
