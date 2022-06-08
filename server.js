179aa206e611f1be2f86227e5164d58fe1b3b398
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-06-08 16:51:42 +0100

    allGardens gives the garden id as well

--- server.js
+++ server.js
@@ -198,7 +198,7 @@ async function handleLogin(req, res) {
 async function handleGetGardensForUser(req, res) {
   const id = req.params.id;
   if (!id) return res.status(400).json({ error: "No id found" });
-  const query = `SELECT location, garden_name FROM gardens WHERE user_id = $1;`;
+  const query = `SELECT id, location, garden_name FROM gardens WHERE user_id = $1;`;
   const gardens = (await client.query(query, [id])).rows;
   res.json(gardens);
 }
