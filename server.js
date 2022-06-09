d4a28f35af830c860aa0ca6dffbb13e2f6f060b2
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-06-09 15:16:25 +0100

    harvest dates ordered

--- server.js
+++ server.js
@@ -103,12 +103,15 @@ async function handleGetHarvestDates(req, res) {
 
   const plants = (
     await client.query(
-      `SELECT name, garden_name, estimated_harvest_date, harvested FROM plants_in_garden
-  JOIN gardens 
-  ON plants_in_garden.garden_id = gardens.id
-  JOIN plant_info
-  ON plants_in_garden.plant_info_id = plant_info.id
-  WHERE user_id = $1`,
+      `SELECT name, garden_name, estimated_harvest_date, harvested 
+      FROM plants_in_garden 
+      JOIN gardens 
+      ON plants_in_garden.garden_id = gardens.id
+      JOIN plant_info
+      ON plants_in_garden.plant_info_id = plant_info.id
+      WHERE user_id = $1
+      ORDER BY estimated_harvest_date 
+      ASC`,
       [userID]
     )
   ).rows;
