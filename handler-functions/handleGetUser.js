8068bf80ef78d1d23f999b1ecffe39f9daef39f4
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-06-10 10:56:42 +0100

    added handleGetUser validation

--- handler-functions/handleGetUser.js
+++ handler-functions/handleGetUser.js
@@ -1,7 +1,12 @@
 async function handleGetUser(req, res, client) {
+  if (!session) res.status(400).json({ error: "No session data found!" });
   const session = req.params.session;
-  const userID = (await client.query(`SELECT user_id FROM sessions WHERE uuid = $1`, [session])).rows[0].user_id;
-  res.status(200).json({ userID });
+  const userIDResponse = await client.query(`SELECT user_id FROM sessions WHERE uuid = $1`, [session]);
+  if (!userIDResponse) res.status(400).json({ error: "No user data found!" });
+  else {
+    userID = userIDResponse.rows[0].user_id;
+    res.status(200).json({ userID });
+  }
 }
 
 module.exports = { handleGetUser };
