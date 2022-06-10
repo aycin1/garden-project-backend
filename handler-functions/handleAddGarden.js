cb4adcf58c27f83db7c6d67b53d39aa7012e375e
aycin1 <aycinkaya98@gmail.com>
2022-06-10 13:50:48 +0100

    adding some testing

--- handler-functions/handleAddGarden.js
+++ handler-functions/handleAddGarden.js
@@ -1,7 +1,11 @@
 async function handleAddGarden(req, res, client) {
   const { location, garden_name, sessionID } = req.body;
-  const id = (await client.query(`SELECT user_id FROM sessions WHERE uuid = $1`, [sessionID])).rows[0]["user_id"];
-  console.log(id);
+  const id = (
+    await client.query(`SELECT user_id FROM sessions WHERE uuid = $1`, [
+      sessionID,
+    ])
+  ).rows[0]["user_id"];
+
   await client.query(
     `INSERT INTO gardens (user_id, garden_name, location)
   VALUES ($1, $2, $3)`,
--- server.js
+++ server.js
@@ -30,13 +30,24 @@ const { Client } = require("pg");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const connection = "postgres://ykwcpoaf:6xDkZhVpuLgvSLqxW-wJSfs_mkVQeyqt@surus.db.elephantsql.com/ykwcpoaf";
+const connection =
+  "postgres://ykwcpoaf:6xDkZhVpuLgvSLqxW-wJSfs_mkVQeyqt@surus.db.elephantsql.com/ykwcpoaf";
 const client = new Client(connection);
 client.connect();
 
 const corsSettings = {
-  origin: ["http://localhost:3000", "https://garden-project-website.sigmalabs.co.uk"],
-  allowedHeaders: ["Access-Control-Allow-Origin", "Authorization", "Content-Type", "Accept", "Origin", "User-Agent"],
+  origin: [
+    "http://localhost:3000",
+    "https://garden-project-website.sigmalabs.co.uk",
+  ],
+  allowedHeaders: [
+    "Access-Control-Allow-Origin",
+    "Authorization",
+    "Content-Type",
+    "Accept",
+    "Origin",
+    "User-Agent",
+  ],
 };
 
 app.use(cookieParser());
@@ -48,30 +59,75 @@ app.use(
   })
 );
 
-app.get("/plants", (req, res) => handleGetPlants.handleGetPlants(req, res, client));
-app.get("/plants/:id", (req, res) => handleGetPlantByID.handleGetPlantByID(req, res, client));
-app.get("/garden/:id", (req, res) => handleGetGarden.handleGetGarden(req, res, client));
-app.get("/shopping-list/:user", (req, res) => handleGetShopping.handleGetShopping(req, res, client));
-app.get("/gardens/:id", (req, res) => handleGetGardensForUser.handleGetGardensForUser(req, res, client));
-app.get("/harvest-dates/:user", (req, res) => handleGetHarvestDates.handleGetHarvestDates(req, res, client));
-app.get("/get-user/:session", (req, res) => handleGetUser.handleGetUser(req, res, client));
-app.get("/allGardens/:id", (req, res) => handleGetGardensForUser.handleGetGardensForUser(req, res, client));
-app.post("/allGardens", (req, res) => handleGetIdAndGardensForUser.handleGetIdAndGardensForUser(req, res, client));
-app.post("/validate-session", (req, res) => handleValidateSession.handleValidateSession(req, res, client));
-app.post("/sign-up", (req, res) => handleRegisterUser.handleRegisterUser(req, res, client, hasher));
-app.post("/login", (req, res) => handleLogin.handleLogin(req, res, client, uuid, hasher));
-app.post("/new-garden", (req, res) => handleAddGarden.handleAddGarden(req, res, client));
-app.post("/new-plant", (req, res) => handleNewPlant.handleNewPlant(req, res, client));
-app.post("/shopping-list", (req, res) => handleAddShopping.handleAddShopping(req, res, client));
-app.patch("/update-plant-status", (req, res) => handlePlanted.handlePlanted(req, res, client));
-app.patch("/harvest", (req, res) => handleHarvest.handleHarvest(req, res, client));
-app.patch("/update-quantity", (req, res) => handleQuantityChange.handleQuantityChange(req, res, client));
-app.patch("/update-bought", (req, res) => handleBoughtChange.handleBoughtChange(req, res, client));
-app.put("/garden/:id", (req, res) => handleUpdateGarden.handleUpdateGarden(req, res, client));
-app.delete("/logout", (req, res) => handleLogout.handleLogout(req, res, client));
-app.delete("/:id/", (req, res) => handleDeletePlants.handleDeletePlants(req, res, client));
+app.get("/plants", (req, res) =>
+  handleGetPlants.handleGetPlants(req, res, client)
+);
+app.get("/plants/:id", (req, res) =>
+  handleGetPlantByID.handleGetPlantByID(req, res, client)
+);
+app.get("/garden/:id", (req, res) =>
+  handleGetGarden.handleGetGarden(req, res, client)
+);
+app.get("/shopping-list/:user", (req, res) =>
+  handleGetShopping.handleGetShopping(req, res, client)
+);
+app.get("/gardens/:id", (req, res) =>
+  handleGetGardensForUser.handleGetGardensForUser(req, res, client)
+);
+app.get("/harvest-dates/:user", (req, res) =>
+  handleGetHarvestDates.handleGetHarvestDates(req, res, client)
+);
+app.get("/get-user/:session", (req, res) =>
+  handleGetUser.handleGetUser(req, res, client)
+);
+app.get("/allGardens/:id", (req, res) =>
+  handleGetGardensForUser.handleGetGardensForUser(req, res, client)
+);
+app.post("/allGardens", (req, res) =>
+  handleGetIdAndGardensForUser.handleGetIdAndGardensForUser(req, res, client)
+);
+app.post("/validate-session", (req, res) =>
+  handleValidateSession.handleValidateSession(req, res, client)
+);
+app.post("/sign-up", (req, res) =>
+  handleRegisterUser.handleRegisterUser(req, res, client, hasher)
+);
+app.post("/login", (req, res) =>
+  handleLogin.handleLogin(req, res, client, uuid, hasher)
+);
+app.post("/new-garden", (req, res) =>
+  handleAddGarden.handleAddGarden(req, res, client)
+);
+app.post("/new-plant", (req, res) =>
+  handleNewPlant.handleNewPlant(req, res, client)
+);
+app.post("/shopping-list", (req, res) =>
+  handleAddShopping.handleAddShopping(req, res, client)
+);
+app.patch("/update-plant-status", (req, res) =>
+  handlePlanted.handlePlanted(req, res, client)
+);
+app.patch("/harvest", (req, res) =>
+  handleHarvest.handleHarvest(req, res, client)
+);
+app.patch("/update-quantity", (req, res) =>
+  handleQuantityChange.handleQuantityChange(req, res, client)
+);
+app.patch("/update-bought", (req, res) =>
+  handleBoughtChange.handleBoughtChange(req, res, client)
+);
+app.put("/garden/:id", (req, res) =>
+  handleUpdateGarden.handleUpdateGarden(req, res, client)
+);
+app.delete("/logout", (req, res) =>
+  handleLogout.handleLogout(req, res, client)
+);
+app.delete("/:id/", (req, res) =>
+  handleDeletePlants.handleDeletePlants(req, res, client)
+);
 app.delete("/shopping-list/:id", (req, res) =>
   handleDeleteShoppingListItem.handleDeleteShoppingListItem(req, res, client)
 );
 
-app.listen(PORT, () => console.log("listening on port " + PORT));
+const server = app.listen(PORT, () => console.log("listening on port " + PORT));
+module.exports = server;
